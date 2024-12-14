ad57b9f26f06c2e0519f4571871b2f96e841fb6f
EVIL210 <g41204270@gmail.com>
2024-12-14 11:29:47 -0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let speech = new SpeechSynthesisUtterance();
+
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', () => {
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech)
+})
